29858fe8269d505cd977a79e23c18968249ad2ae
MANOJ KUMAR <manojkumar06t@gmail.com>
2024-01-26 23:05:51 +0530

    adding env and mongodb

--- configs/dbconnection.js
+++ configs/dbconnection.js
@@ -1,10 +1,11 @@
-
+require('dotenv').config();
 const mongoose = require("mongoose")
 
 // Making Connection with mongoDB
 const connectDb = async ()=>{
     try{
-        const connect = await mongoose.connect("mongodb://localhost/issue_tracker");
+        //const connect = await mongoose.connect("mongodb://localhost/issue_tracker");
+        const connect = await mongoose.connect(process.env.MONGO_URL);
         console.log("Database Connected: ", connect.connection.name)
     }
     catch(err){
